09b822293e84baa114646517da245a745cc299fe
GM Huang <61339305+Jamtech77@users.noreply.github.com>
2021-04-06 18:33:16 +0800

    Update getcolorcode.py

--- getcolorcode.py
+++ getcolorcode.py
@@ -14,9 +14,25 @@ input_thread = threading.Thread(target=wait_for_input)
 input_thread.start()
 
 while 1:
-    if pause_flag == 0:
+    if pause_flag < 2:
         curPOS = pygui.position()
         im = pygui.screenshot()
         pix = im.getpixel((curPOS[0], curPOS[1]))
-        print(pix)
-        time.sleep(0.5)
+
+        output = ''
+
+        if pause_flag == 1:
+            # print(hex(pix[0]).replace('0x', ''), hex(pix[1]).replace('0x', ''), hex(pix[2]).replace('0x', ''))
+            print(pix)
+        else:
+            for color in pix:
+                color_hex = hex(color).replace('0x', '') + ' '
+                if color < 16:
+                    color_hex = '0' + color_hex
+                output += color_hex
+
+            print(output)
+
+        time.sleep(0.1)
+
+        
